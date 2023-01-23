542850286bd845a8706bc5f06e146e6066d09ef8
Kiril Isakov <kirix.isakov@gmail.com>
2023-01-23 20:09:22 +0100

    update

--- url_clean.go
+++ url_clean.go
@@ -9,14 +9,17 @@ import (
 )
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	// http://localhost:3000/url_clean?url=https://example.com?utm_campaign=a.coronavirus-special-edition&utm_medium=email.internal-newsletter.np&utm_source=salesforce-marketing-cloud&utm_term=20230121&utm_content=ed-picks-article-link-6&etear=nl_special_6&utm_campaign=a.coronavirus-special-edition&utm_medium=email.internal-newsletter.np&utm_source=salesforce-marketing-cloud&utm_term=1/21/2023&utm_id=1456534
+	// test:
+	// curl http://localhost:3000/url_clean?url=https://www.economist.com/christmas-specials/2020/12/19/awesome-weird-and-everything-else?utm_campaign=a.coronavirus-special-edition&utm_medium=email.internal-newsletter.np&utm_source=salesforce-marketing-cloud&utm_term=20230121&utm_content=ed-picks-article-link-6&etear=nl_special_6&utm_campaign=a.coronavirus-special-edition&utm_medium=email.internal-newsletter.np&utm_source=salesforce-marketing-cloud&utm_term=1/21/2023&utm_id=1456534
 	router.GET("/url_clean", func(c *gin.Context) {
 		url_to_clean := c.Query("url")
 		clean_url := url_cleaner.Clean_url_from_unwanted_query_params(url_to_clean)
 		c.String(http.StatusOK, clean_url+"\n")
 	})
-	// http://localhost:3000/url_clean/https://example.com?utm_campaign=a.coronavirus-special-edition&utm_medium=email.internal-newsletter.np&utm_source=salesforce-marketing-cloud&utm_term=20230121&utm_content=ed-picks-article-link-6&etear=nl_special_6&utm_campaign=a.coronavirus-special-edition&utm_medium=email.internal-newsletter.np&utm_source=salesforce-marketing-cloud&utm_term=1/21/2023&utm_id=1456534
+	// test:
+	// curl http://localhost:3000/url_clean/https://www.economist.com/christmas-specials/2020/12/19/awesome-weird-and-everything-else?utm_campaign=a.coronavirus-special-edition&utm_medium=email.internal-newsletter.np&utm_source=salesforce-marketing-cloud&utm_term=20230121&utm_content=ed-picks-article-link-6&etear=nl_special_6&utm_campaign=a.coronavirus-special-edition&utm_medium=email.internal-newsletter.np&utm_source=salesforce-marketing-cloud&utm_term=1/21/2023&utm_id=1456534
 	router.GET("/url_clean/*url", func(c *gin.Context) {
 		url_to_clean := c.Param("url")[1:]
 		clean_url := url_cleaner.Clean_url_from_unwanted_query_params(url_to_clean)
